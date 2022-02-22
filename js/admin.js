60f4fa4dfc73541a0083b90058d4e97c71d94e16
unknown <hunkclement@gmail.com>
2022-02-22 14:20:39 +0200

    sending user as response

--- js/admin.js
+++ js/admin.js
@@ -17,7 +17,7 @@ displayuser=(users)=>{
 
 document.getElementById("users").innerHTML =`
 ${users.map(function(user){
-    return `<div><h4>SECONDNAME: ${user.lastname}<br> USERNAME: ${user.username}<br> EMAIL: ${user.email}<br> role: ${user.role}</h4><br><button type="submit" onclick="deleteuser(value)" value="${user._id}" id="sendcmt">delete</button></div>`
+    return `<div style="margin-left:100px;margin-top:-30px;width:300px"><h4>SECONDNAME: ${user.lastname}<br> USERNAME: ${user.username}<br> EMAIL: ${user.email}<br> role: ${user.role}</h4><br><button type="submit" onclick="deleteuser(value)" value="${user._id}" id="sendcmt">delete</button></div>`
 }).join('')}
 `
 }
